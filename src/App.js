8752a39954c9be120c7e481cf3f3bf7b07f59eeb
MathFreitas <matheus.freitas_santos@outlook.com>
2022-03-30 16:58:19 -0300

    estilizando a page e adicionando icons com react-native-vector-icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, SafeAreaView } from "react-native";
+import Icon from 'react-native-vector-icons/Feather';
+
+import foto from './assets/foto.jpeg';
 
 const App = () => {
     return (
@@ -7,12 +10,12 @@ const App = () => {
             <View style={style.page}>
                 <View style={style.container_cabecalho} >
                     <Image source={foto} style={style.foto} />
-                    <Text>MATHEUS FREITAS</Text>
-                    <Text>Desenvolvedor Front-end</Text>
-                    <View>
-                        <Text>Github</Text>
-                        <Text>Facebook</Text>
-                        <Text>Linkedin</Text>
+                    <Text style={style.nome}>MATHEUS FREITAS</Text>
+                    <Text style={style.funcao}>Desenvolvedor Front-end</Text>
+                    <View style={style.redes_sociais}>
+                        <Icon name="github" size={30}/>
+                        <Icon name="facebook" size={30}/>
+                        <Icon name="linkedin" size={30}/>
                     </View>
                 </View>
             </View>
@@ -26,14 +29,29 @@ const style = StyleSheet.create({
         flex: 1
     },
     container_cabecalho:{
-        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: 50
     },
     foto:{
         width: 250,
         height: 250,
         borderRadius: 125
+    },
+    nome: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginTop: 10
+    },
+    funcao: {
+        color: '#939393',
+        marginBottom: 10
+    },
+    redes_sociais: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%',
+        marginTop: 20
     }
 })
 
